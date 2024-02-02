f798213877eb20441c51d5f97538df469e2e1229
Alieu Friggeri <alieufriggeri00@gmail.com>
2024-02-02 15:04:03 +0100

    mdrrrrrr

--- req/mariadb/script/script.sh
+++ req/mariadb/script/script.sh
@@ -6,12 +6,12 @@ set -x
 touch file
 chmod 777 file
 
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" >> file
+echo "CREATE DATABASE IF NOT EXISTS wordpress;" >> file
 echo "FLUSH PRIVILEGES;" >> file
 echo "GRANT ALL ON *.* TO '$MYSQL_ROOT_USER'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;" >> file
 echo "FLUSH PRIVILEGES;" >> file
-echo "CREATE USER IF NOT EXISTS '$MYSQL_ROOT_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> file
-echo "GRANT ALL ON wordpress.* TO '$MYSQL_ROOT_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"  >> file
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> file
+echo "GRANT ALL ON wordpress.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"  >> file
 echo "FLUSH PRIVILEGES;" >> file
 
 cat file
